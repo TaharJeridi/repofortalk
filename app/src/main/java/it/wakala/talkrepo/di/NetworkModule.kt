458b0fb0b4dc3f9ec3a201a92f0a3aec2426e64f
Mirco Landriani <m.landriani@wakala.it>
2022-02-21 17:36:16 +0100

    Hilt Network Module

---
+++ app/src/main/java/it/wakala/talkrepo/di/NetworkModule.kt
@@ -0,0 +1,65 @@
+package it.wakala.talkrepo.di
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import it.wakala.talkrepo.BuildConfig
+import it.wakala.talkrepo.api.RemoteApi
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(@ApplicationContext context: Context, okHttpLoggingInterceptor: HttpLoggingInterceptor, cache: Cache): OkHttpClient {
+        return OkHttpClient.Builder()
+            .cache(cache)
+            .addInterceptor { chain ->
+                val request = chain.request()
+                val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+                if (connectivityManager.activeNetwork != null) {
+                    request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+                } else {
+                    request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+                }
+                chain.proceed(request)
+            }
+            .addInterceptor(okHttpLoggingInterceptor).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create())
+        .baseUrl(BuildConfig.SERVER_URL)
+        .client(okHttpClient)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideApi(retrofit: Retrofit): RemoteApi =
+        retrofit.create(RemoteApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClientCache(@ApplicationContext context: Context): Cache =
+        Cache(context.cacheDir, (10 * 1024 * 1024).toLong())
+
+}
