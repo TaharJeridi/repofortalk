7d830fb82899de8a4759279f407dab22caddbb35
Tahar <t.jeridi@wakala.it>
2022-02-27 18:04:56 +0100

    add baseViewModel.

--- app/src/main/java/it/wakala/talkrepo/base/ABaseViewModel.kt
+++ app/src/main/java/it/wakala/talkrepo/base/ABaseViewModel.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
 
-class ABaseViewModel(application: Application) : AndroidViewModel(application) {
+open class ABaseViewModel(application: Application) : AndroidViewModel(application) {
 
     var exceptionHandler = CoroutineExceptionHandler { _, exception ->
     }
