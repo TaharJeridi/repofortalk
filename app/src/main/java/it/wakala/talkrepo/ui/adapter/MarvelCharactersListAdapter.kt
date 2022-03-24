9cf676123197be5eec9ab1df9ad3ff02a37626ce
Mirco Landriani <m.landriani@wakala.it>
2022-03-24 14:50:14 +0100

    Marvel characters list refactor

--- app/src/main/java/it/wakala/talkrepo/ui/adapter/MarvelCharactersListAdapter.kt
+++ app/src/main/java/it/wakala/talkrepo/ui/adapter/MarvelCharactersListAdapter.kt
@@ -3,93 +3,57 @@ package it.wakala.talkrepo.ui.adapter
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import it.wakala.talkrepo.databinding.ItemViewLoadingBinding
 import it.wakala.talkrepo.databinding.ItemViewMarvelCharacterBinding
 import it.wakala.talkrepo.entity.ResultEntity
+import it.wakala.talkrepo.modelview.ComicsModelView
 import it.wakala.talkrepo.ui.state.Loading
 import it.wakala.talkrepo.ui.uimodel.MarvelCharactersResult
 import it.wakala.talkrepo.ui.uimodel.ResultLoadingUiModel
 import it.wakala.talkrepo.ui.uimodel.ResultUiModel
 
-class MarvelCharactersListAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class MarvelCharactersListAdapter: PagingDataAdapter<MarvelCharactersResult, RecyclerView.ViewHolder>(diffCallback) {
 
     companion object {
-        const val LOADING = 0
-        const val DATA = 1
-    }
+        val diffCallback = object : DiffUtil.ItemCallback<MarvelCharactersResult>() {
+            override fun areItemsTheSame(
+                oldItem: MarvelCharactersResult,
+                newItem: MarvelCharactersResult
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
 
-    var data: MutableList<MarvelCharactersResult> = mutableListOf()
-    @SuppressLint("NotifyDataSetChanged")
-    set(value) {
-        field.addAll(value)
-        notifyDataSetChanged()
+            override fun areContentsTheSame(
+                oldItem: MarvelCharactersResult,
+                newItem: MarvelCharactersResult
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-
-        return when(viewType) {
-            LOADING -> {
-                val binding = ItemViewLoadingBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-                LoadingViewHolder(binding)
-            }
-            else -> {
-                val binding = ItemViewMarvelCharacterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-                MarvelCharactersViewHolder(binding)
-            }
-        }
-
+        val binding = ItemViewMarvelCharacterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+       return  MarvelCharactersViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
-        when(holder) {
-            is MarvelCharactersViewHolder -> holder.render(data[position])
-            else -> (holder as? LoadingViewHolder)?.render()
-        }
-
-
-    }
-
-    override fun getItemCount(): Int {
-        return data.size
-    }
-
-    fun addLoading() {
-        data.add(ResultLoadingUiModel)
-    }
-
-    fun removeLoading() {
-        data.removeAll {it is ResultLoadingUiModel}
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return when(data[position]) {
-            is ResultLoadingUiModel -> {
-                LOADING
-            }
-            else -> DATA
-        }
+        (holder as MarvelCharactersViewHolder).render(getItem(position))
     }
 
     inner class MarvelCharactersViewHolder(val binding: ItemViewMarvelCharacterBinding): RecyclerView.ViewHolder(binding.root) {
-        fun render(value: MarvelCharactersResult) {
+        fun render(value: MarvelCharactersResult?) {
             with(binding) {
                 Glide.with(heroIv)
-                    .load("${value.thumbnail?.path}.${value.thumbnail?.extension}")
+                    .load("${value?.thumbnail?.path}.${value?.thumbnail?.extension}")
                     .circleCrop()
                     .into(heroIv)
 
-                heroNameTv.text = value.name?.replaceFirstChar { it.uppercase() }
-            }
-        }
-    }
-
-    inner class LoadingViewHolder(private val binding: ItemViewLoadingBinding): RecyclerView.ViewHolder(binding.root) {
-        fun render() {
-            with(binding) {
-                loadingContainer.startShimmer()
+                heroNameTv.text = value?.name?.replaceFirstChar { it.uppercase() }
             }
         }
     }
--- app/src/main/java/it/wakala/talkrepo/ui/fragment/HomeFragment.kt
+++ app/src/main/java/it/wakala/talkrepo/ui/fragment/HomeFragment.kt
@@ -5,9 +5,7 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import it.wakala.talkrepo.base.ABaseFragment
-import it.wakala.talkrepo.base.StatefulData
 import it.wakala.talkrepo.databinding.HomeFragmentBinding
-import it.wakala.talkrepo.ext.gridPagination
 import it.wakala.talkrepo.ui.adapter.MarvelCharactersListAdapter
 import it.wakala.talkrepo.ui.viewmodel.MarvelCharactersViewModel
 import timber.log.Timber
@@ -23,42 +21,20 @@ class HomeFragment : ABaseFragment<HomeFragmentBinding>() {
         return HomeFragmentBinding.inflate(layoutInflater)
     }
 
-    private var isLoading = false
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         with(binding) {
             marvelCharactersList.adapter = mAdapter
-            marvelCharactersList.gridPagination() {
-                if (!isLoading) marvelCharactersViewModel.getMarvelCharactersList(true)
-            }
-
-        }
-
-        marvelCharactersViewModel.marvelCharactersLiveData.observe(viewLifecycleOwner) {
-
-            val marvelCharsResults = it.getOrNull() ?: return@observe
-            when (marvelCharsResults) {
-                is StatefulData.Success -> {
-                    isLoading = false
-                    mAdapter.removeLoading()
-                    mAdapter.data = marvelCharsResults.result.toMutableList()
-                }
-                is StatefulData.Loading -> {
-                    isLoading = true
-                    mAdapter.addLoading()
-                    binding.marvelCharactersList.smoothScrollToPosition(mAdapter.data.size - 1)
-                }
-                else -> {}
-            }
         }
 
         marvelCharactersViewModel.errorLiveData.observe(viewLifecycleOwner) {
             Timber.e(it.error)
         }
 
-        marvelCharactersViewModel.getMarvelCharactersList()
+        marvelCharactersViewModel.fetchMarvelCharactersList().observe(viewLifecycleOwner) {
+            mAdapter.submitData(lifecycle, it)
+        }
 
     }
 
